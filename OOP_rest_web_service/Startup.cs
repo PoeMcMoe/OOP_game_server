c83cc7d800ddbbdc4efa8412fc6e68b769f7e80a
Vytenis Kunickas <vytenis2008@gmail.com>
2020-10-25 15:16:37 +0200

    Prideta collisionu aptikimas tarp food'o ir playeriu, neistestuota

--- OOP_rest_web_service/Startup.cs
+++ OOP_rest_web_service/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using OOP_rest_web_service.Controllers;
+using OOP_rest_web_service.Models;
 
 namespace OOP_rest_web_service
 {
@@ -20,6 +21,7 @@ namespace OOP_rest_web_service
     {
         static List<Color> allColors;
         public static List<DateTime> lastPosts;
+        int sizeChange = 1;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -45,6 +47,7 @@ namespace OOP_rest_web_service
             lastPosts.Add(new DateTime());
 
             PeriodicMapCheck(TimeSpan.FromSeconds(0.1));
+            PeriodicCollisionCheck(TimeSpan.FromSeconds(0.1));
         }
 
         public IConfiguration Configuration { get; }
@@ -80,6 +83,59 @@ namespace OOP_rest_web_service
                     if (DateTime.Now.Subtract(lastPosts[i]) >= TimeSpan.FromSeconds(5))
                     {
                         GameController.map.removePlayers(i);
+                    }
+                }
+                await Task.Delay(interval);
+            }
+        }
+
+        public async Task PeriodicCollisionCheck(TimeSpan interval)
+        {
+            while (true)
+            {
+                List<Unit> players = GameController.map.getPlayers();
+                List<Unit> food = GameController.map.getFood();
+
+                for (int i = 0; i < 8; i++)
+                {
+                    if (players[i] != null)
+                    {
+                        int x1 = players[i].position.X - players[i].playerSize.Width / 2;
+                        int x2 = players[i].position.X + players[i].playerSize.Width / 2;
+                        int y1 = players[i].position.Y - players[i].playerSize.Height / 2;
+                        int y2 = players[i].position.Y - players[i].playerSize.Height / 2;
+
+                        for(int j = 0; j < food.Count; j++)
+                        {
+                            if(food[j].position.X >= x1 && food[j].position.X <= x2 && food[j].position.Y >= y1 && food[j].position.Y >= y2)
+                            {
+                                GameController.map.removeFood(j);
+                                players[i].playerSize = new Size(players[i].playerSize.Width + 1, players[i].playerSize.Height + 1);
+                                GameController.map.setPlayer(i, players[i]);
+                            }
+                        }
+
+                        for (int j = 0; j < 8; j++)
+                        {
+                            if(i != j)
+                            {
+                                if (players[j].position.X >= x1 && players[j].position.X <= x2 && players[j].position.Y >= y1 && players[j].position.Y >= y2)
+                                {
+                                    if(players[i].playerSize.Width > players[j].playerSize.Width)
+                                    {
+                                        GameController.map.removePlayers(j);
+                                        players[i].playerSize = new Size(players[i].playerSize.Width + 1, players[i].playerSize.Height + 1);
+                                        GameController.map.setPlayer(i, players[i]);
+                                    }
+                                    else if(players[i].playerSize.Width < players[j].playerSize.Width)
+                                    {
+                                        GameController.map.removePlayers(i);
+                                        players[j].playerSize = new Size(players[j].playerSize.Width + 1, players[j].playerSize.Height + 1);
+                                        GameController.map.setPlayer(j, players[j]);
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
                 await Task.Delay(interval);
